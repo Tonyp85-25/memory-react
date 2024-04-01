2ab7170a855c6d5691d24ee86ac5b8e291639ded
Tonyp85-25 <anthony-paulin@etudiant.oclock.io>
2024-04-01 18:21:42 +0200

    Add Vitest and remove jest

--- src/components/tests/timer.test.tsx
+++ src/components/tests/timer.test.tsx
@@ -3,52 +3,52 @@ import Timer, { GAME_DURATION } from "../Timer";
 
 const originalError = console.error;
 beforeAll(() => {
-	console.error = (...args) => {
-		if (/Warning.*not wrapped in act/.test(args[0])) {
-			return;
-		}
-		originalError.call(console, ...args);
-	};
+  console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
 });
 
 afterAll(() => {
-	console.error = originalError;
+  console.error = originalError;
 });
 beforeEach(() => {
-	jest.useFakeTimers();
+  vi.useFakeTimers();
 });
 afterEach(() => {
-	jest.runOnlyPendingTimers();
-	jest.useRealTimers();
+  vi.runOnlyPendingTimers();
+  vi.useRealTimers();
 });
 describe("progress bar gets filled according to difficulty level duration", () => {
-	test("progress bar gets filled in one minute in easy level", async () => {
-		render(<Timer difficulty={"easy"} />, "easy");
-		jest.advanceTimersByTime(GAME_DURATION.easy);
-		const progressBar = screen.getByRole("progressbar");
+  test("progress bar gets filled in one minute in easy level", async () => {
+    render(<Timer difficulty={"easy"} />, "easy");
+    vi.advanceTimersByTime(GAME_DURATION.easy);
+    const progressBar = screen.getByRole("progressbar");
 
-		expect(progressBar.getAttribute("value")).toBe("100");
-	});
-	test("progress bar gets filled in one minute and half in hard level", () => {
-		render(<Timer difficulty={"hard"} />, "hard");
-		jest.advanceTimersByTime(GAME_DURATION.hard);
-		const progressBar = screen.getByRole("progressbar");
+    expect(progressBar.getAttribute("value")).toBe("100");
+  });
+  test("progress bar gets filled in one minute and half in hard level", () => {
+    render(<Timer difficulty={"hard"} />, "hard");
+    vi.advanceTimersByTime(GAME_DURATION.hard);
+    const progressBar = screen.getByRole("progressbar");
 
-		expect(progressBar.getAttribute("value")).toBe("100");
-	});
+    expect(progressBar.getAttribute("value")).toBe("100");
+  });
 });
 
 describe("should display text when time is up", () => {
-	test("should display text when easy time is up", async () => {
-		render(<Timer difficulty={"easy"} />, "easy");
-		jest.advanceTimersByTime(GAME_DURATION.easy);
-		const text = await screen.findByText("Time's up!");
-		expect(text).toBeInTheDocument();
-	});
-	test("should display text when hard time is up", async () => {
-		render(<Timer difficulty={"hard"} />, "hard");
-		jest.advanceTimersByTime(GAME_DURATION.hard);
-		const text = await screen.findByText("Time's up!");
-		expect(text).toBeInTheDocument();
-	});
+  test("should display text when easy time is up", async () => {
+    render(<Timer difficulty={"easy"} />, "easy");
+    vi.advanceTimersByTime(GAME_DURATION.easy);
+    const text = await screen.findByText("Time's up!");
+    expect(text).toBeInTheDocument();
+  });
+  test("should display text when hard time is up", async () => {
+    render(<Timer difficulty={"hard"} />, "hard");
+    vi.advanceTimersByTime(GAME_DURATION.hard);
+    const text = await screen.findByText("Time's up!");
+    expect(text).toBeInTheDocument();
+  });
 });
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import svgrPlugin from "vite-plugin-svgr";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  build: {
+    outDir: "build",
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+  plugins: [
+    react(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+  ],
+});
